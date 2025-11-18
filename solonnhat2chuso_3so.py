0717a46c9ee8968a67e3e6314155af21561c1a3a
nhith2025 <nhith7@gmail.com>
2025-11-18 19:22:28 +0700

    Create solonnhat2chuso_3so.py

---
+++ solonnhat2chuso_3so.py
@@ -0,0 +1,23 @@
+n=int(input())
+a=n%10
+n=n//10
+b=n%10
+c=n//10
+so1=10*a+b
+so2=10*b+c
+so3=10*b+a
+so4=10*b+c
+so5=10*c+a
+so6=10*c+b
+lonnhat=so1
+if so2>lonnhat:
+    lonnhat=so2
+if so3>lonnhat:
+    lonnhat=so3
+if so4>lonnhat:
+    lonnhat=so4
+if so5>lonnhat:
+    lonnhat=so5
+if so6>lonnhat:
+    lonnhat=so6
+print(lonnhat)
