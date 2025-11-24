1f502801f454a0c039b428714279898c26a899fd
nhith2025 <nhith7@gmail.com>
2025-11-24 15:59:34 +0700

    Create canhtamgiac.py

---
+++ canhtamgiac.py
@@ -0,0 +1,10 @@
+import math
+a=int(input())
+b=int(input())
+c=int(input())
+if a+b>c and a+c>b and b+c>a:
+    p=(a+b+c)/2
+    s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+    print(s)
+else:
+    print(-1)
